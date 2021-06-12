9bc44a33b17562d6527122bab30c62e28b1589b8
anabrd <ana.d.brdar@gmail.com>
2021-06-12 15:15:16 +0200

    update imgpath

--- controller/contacts.js
+++ controller/contacts.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, '/static/media')
+        callback(null, __dirname + '/static/media')
     },
     filename: function(req, file, callback) {
         // The timestamps will give a unique filename on the server
