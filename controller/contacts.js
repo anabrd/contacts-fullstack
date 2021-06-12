a34c9f40359a35cb360ddbd35e8c8b67a403f59f
anabrd <ana.d.brdar@gmail.com>
2021-06-12 14:54:39 +0200

    update imgpath

--- controller/contacts.js
+++ controller/contacts.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, 'public/avatars')
+        callback(null, 'avatars')
     },
     filename: function(req, file, callback) {
         // The timestamps will give a unique filename on the server
@@ -55,7 +55,7 @@ exports.deleteContact = (req, res) => {
             })
 
             try {
-                fs.unlinkSync('public/avatars/' + doc.contactPic);
+                fs.unlinkSync('avatars/' + doc.contactPic);
                 console.log("deleted")
             } catch (err) {
                 // handle the error
@@ -90,7 +90,7 @@ exports.updateContact = (req,res) => {
         } else {
             if (contact.contactPic) {
                 try {
-                    fs.unlinkSync('public/avatars/' + doc.contactPic);
+                    fs.unlinkSync('avatars/' + doc.contactPic);
                     console.log("deleted")
                 } catch (err) {
                     // handle the error
